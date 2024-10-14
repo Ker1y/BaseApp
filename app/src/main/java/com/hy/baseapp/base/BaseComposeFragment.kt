734dbccd3603e503fa63c05e97b9836d18a5d9ce
@Hy <krishu@foxmail.com>
2024-10-14 18:09:06 +0800

    加入BaseComposeFragment

---
+++ app/src/main/java/com/hy/baseapp/base/BaseComposeFragment.kt
@@ -0,0 +1,36 @@
+package com.hy.baseapp.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import me.hy.jetpackmvvm.base.fragment.BaseVmFragment
+import me.hy.jetpackmvvm.base.viewmodel.BaseViewModel
+
+abstract class BaseComposeFragment<VM : BaseViewModel> : BaseVmFragment<VM>() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val composeView = ComposeView(requireContext())
+        composeView.setContent {
+            AppTheme {
+                SetContent()
+            }
+        }
+        return composeView
+    }
+
+    @Composable
+    abstract fun SetContent(content: @Composable () -> Unit = {})
+
+    override fun initObserver() {}
+    override fun initClick() {}
+    override fun layoutId(): Int = 0
+    override fun initView(savedInstanceState: Bundle?) {}
+    override fun lazyLoadData() {}
+}
---
+++ app/src/main/java/com/hy/baseapp/ui/home/TestFragment.kt
@@ -0,0 +1,14 @@
+package com.hy.baseapp.ui.home
+
+import androidx.compose.runtime.Composable
+import com.hy.baseapp.base.BaseComposeFragment
+import me.hy.jetpackmvvm.base.viewmodel.BaseViewModel
+
+class TestFragment : BaseComposeFragment<BaseViewModel>() {
+
+    @Composable
+    override fun SetContent(content: @Composable () -> Unit) {
+
+    }
+
+}
